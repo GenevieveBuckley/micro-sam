ab95f5e5ab7d5c8ed0db7d121b8bca2fe9dfe000
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2024-02-06 12:59:49 +1100

    Skip failing test on github actions

--- test/test_vendored.py
+++ test/test_vendored.py
@@ -18,17 +18,12 @@ class TestVendored(unittest.TestCase):
 
     def _test_batched_mask_to_box(self, device):
         from micro_sam._vendored import batched_mask_to_box
-        print("Torch current allocated memory:")
-        print(torch.mps.current_allocated_memory())
-        os.environ['PYTORCH_MPS_HIGH_WATERMARK_RATIO'] = '0.0'
+
         mask, expected_result = self._get_mask_to_box_data()
-        print("Torch current allocated memory:")
-        print(torch.mps.current_allocated_memory())
         mask = torch.as_tensor(mask, dtype=torch.bool, device=device)
         expected_result = torch.as_tensor(expected_result, dtype=torch.int, device=device)
         result = batched_mask_to_box(mask)
         assert all(result == expected_result)
-        os.environ.pop('PYTORCH_MPS_HIGH_WATERMARK_RATIO')
 
     def test_cpu_batched_mask_to_box(self):
         self._test_batched_mask_to_box(device="cpu")
@@ -39,6 +34,9 @@ class TestVendored(unittest.TestCase):
         torch.cuda.empty_cache()
         self._test_batched_mask_to_box(device="cuda")
 
+    @unittest.skipif((os.getenv("GITHUB_ACTIONS") == "true"),
+                     "Test fails on Github Actions macos-14 runner " + \
+                     "https://github.com/computational-cell-analytics/micro-sam/issues/380")
     @unittest.skipIf(not (torch.backends.mps.is_available() and torch.backends.mps.is_built()),
                      "MPS Pytorch backend is not available")
     def test_mps_batched_mask_to_box(self):
