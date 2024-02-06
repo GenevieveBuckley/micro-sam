697a9137794eb0bffa61835b033dd939bd2fde2b
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2024-02-06 12:38:10 +1100

    Try patching os.environ dict with PYTORCH_MPS_HIGH_WATERMARK_RATIO

--- test/test_vendored.py
+++ test/test_vendored.py
@@ -2,6 +2,7 @@ import unittest
 
 import numpy as np
 import torch
+from unittest.mock import patch
 
 from segment_anything.utils.amg import mask_to_rle_pytorch as mask_to_rle_pytorch_sam
 from skimage.draw import random_shapes
@@ -14,10 +15,15 @@ class TestVendored(unittest.TestCase):
         expected_result = [3, 7, 4, 8]
         return mask_numpy, expected_result
 
+
+    @patch.dict('os.environ', {'PYTORCH_MPS_HIGH_WATERMARK_RATIO': '0.0'})
     def _test_batched_mask_to_box(self, device):
         from micro_sam._vendored import batched_mask_to_box
-
+        print("Torch current allocated memory:")
+        print(torch.mps.current_allocated_memory())
         mask, expected_result = self._get_mask_to_box_data()
+        print("Torch current allocated memory:")
+        print(torch.mps.current_allocated_memory())
         mask = torch.as_tensor(mask, dtype=torch.bool, device=device)
         expected_result = torch.as_tensor(expected_result, dtype=torch.int, device=device)
         result = batched_mask_to_box(mask)
